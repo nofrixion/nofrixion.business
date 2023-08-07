24a7ce1245bb7034d0d351c395837dd4cb0f8bc2
Donal O'Connor <donal@nofrixion.com>
2023-08-07 10:43:56 +0100

    Renamed to nofrixion.components

--- packages/nofrixion.webcomponents/src/index.ts
+++
@@ -1,7 +0,0 @@
-import './index.css' // Import Tailwind CSS
-
-// Web Components that want to be built
-// need to be imported here
-// import './web-components/PaymentRequestDashboard/WebPaymentRequestDashboard';
-
-export { default as Dashboard } from './components/functional/PaymentRequestDashboard/PaymentRequestDashboard'
--- packages/nofrixion.webcomponents/src/utils/validation.ts
+++
@@ -1,6 +0,0 @@
-const validateEmail = (email: string) => {
-  const re = /\S+@\S+\.\S+/
-  return re.test(email)
-}
-
-export { validateEmail }
