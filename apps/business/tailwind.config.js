cf4204a240663f08bfb3193e3c350906f1bd8089
Donal O'Connor <donal@nofrixion.com>
2023-08-09 18:09:07 +0100

    Created shared tailwind-config

---
+++ apps/business/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+
+import sharedConfig from '@nofrixion/tailwind-config/tailwind.config.cjs'
+
+module.exports = {
+  presets: [sharedConfig],
+  prefix: 'biz-',
+}
