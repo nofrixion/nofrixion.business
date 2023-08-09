1925ad72f1dc1f236ae56f455cd8e402f72762f7
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-08-09 08:48:21 +0100

    Fixed css prefix issue

--- packages/webcomponents/vite.config.ts
+++ packages/webcomponents/vite.config.ts
@@ -1,24 +1,12 @@
 import { resolve } from 'path'
-import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import banner from 'vite-plugin-banner'
-import { vitePlugin as utwm } from 'unplugin-tailwindcss-mangle'
-
-const builtClassesPrefix = 'nf-wc-'
 
 export default defineConfig({
   plugins: [
     react(),
-    utwm({
-      classGenerator: {
-        classPrefix: builtClassesPrefix,
-        customGenerate: (original, options) => {
-          return options.classPrefix + original
-        },
-      },
-    }),
     cssInjectedByJsPlugin(),
     banner(`NoFrixion Web Components - Version ${process.env.npm_package_version}`),
   ],
@@ -30,9 +18,5 @@ export default defineConfig({
       name: 'web-components',
       fileName: () => `web-components.js`,
     },
-  },
-  define: {
-    'process.env': `"${process.env}"`,
-    builtClassesPrefix: `"${builtClassesPrefix}"`,
   },
 })
