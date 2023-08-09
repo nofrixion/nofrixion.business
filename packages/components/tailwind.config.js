31c5cf8c71097e88fd18dd8f68826217a32adea9
Donal O'Connor <donal@nofrixion.com>
2023-08-09 17:58:30 +0100

    Created shared tailwind-config

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 import sharedConfig from '@nofrixion/tailwind-config/tailwind.config.cjs'
 
 export const presets = [sharedConfig]
