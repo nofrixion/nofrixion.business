978aa422f24bdacf2a48a04a33e3bcb6f695bbe2
arifmatin <arif@nofrixion.com>
2023-08-16 11:29:16 +0100

    Fix size for storybook

--- packages/components/src/components/ui/Account/CurrentAccountsHeader/CurrentAccountsHeader .tsx
+++ packages/components/src/components/ui/Account/CurrentAccountsHeader/CurrentAccountsHeader .tsx
@@ -10,10 +10,12 @@ const CurrentAccountsHeader = ({ onCreatePaymentAccount }: CurrentAccountsHeader
       <span className="md:pl-4 leading-8 font-medium text-2xl md:text-[1.75rem]">
         Currents accounts
       </span>
-      <Button size="big" onClick={onCreatePaymentAccount} variant="secondary" className="w-[172px]">
-        <Icon name="add/16" className="text-default-text" />
-        <span className="pl-2 md:inline-block">New Account</span>
-      </Button>
+      <div className="w-[172px]">
+        <Button size="big" onClick={onCreatePaymentAccount} variant="secondary">
+          <Icon name="add/16" className="text-default-text" />
+          <span className="pl-2 md:inline-block">New Account</span>
+        </Button>
+      </div>
     </div>
   )
 }
