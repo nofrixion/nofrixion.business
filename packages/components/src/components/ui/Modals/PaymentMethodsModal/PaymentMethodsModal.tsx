7093376a98446cf24cc8788c7cc5ea0b37cb3f93
Axel Granillo <axel@nofrixion.com>
2023-08-21 08:44:28 -0600

    🐛 Fixed wrong font size on payment method modal

--- packages/components/src/components/ui/Modals/PaymentMethodsModal/PaymentMethodsModal.tsx
+++ packages/components/src/components/ui/Modals/PaymentMethodsModal/PaymentMethodsModal.tsx
@@ -191,7 +191,7 @@ const PaymentMethodsModal = ({
         isWalletEnabled && !isCardEnabled && !isBankEnabled && !isLightningEnabled ? 'md:mt-6' : ''
       }
     >
-      <div className="[&>*]:border-b [&>*]:border-solid [&>*]:border-b-border-grey">
+      <div className="[&>*]:border-b [&>*]:border-solid [&>*]:border-b-border-grey text-sm">
         <Switch
           icon={ApplePayIcon}
           label="Apple Pay / Google Pay"
@@ -279,7 +279,7 @@ const PaymentMethodsModal = ({
         </div>
         <div className="py-6 md:py-4 w-full select-none">
           <img src={BitcoinIcon} alt="Bitcoin Lightning" className="w-6 h-6 mr-4 inline-block" />
-          <span className="align-middle pr-2 text-grey-text">Bitcoin Lightning (coming soon)</span>
+          <span className="align-middle pr-2 text-grey-text text-sm">Bitcoin Lightning (coming soon)</span>
         </div>
       </div>
 
