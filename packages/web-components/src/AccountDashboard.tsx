457318cec9fee5fe2ddfc9480e7d5e4a0817aeef
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2023-08-30 15:04:40 -0300

    ✨ feat: add `account-dashboard` web component
    - The `account-dashboard` web component was added.
    - Successfully tested the component locally by integrating it into the Portal.
    - A subsequent PR will be created on the MoneyMoov Portal (API solution) to incorporate the new Account Dashboard.

---
+++ packages/web-components/src/AccountDashboard.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client'
+import { AccountDashboard } from '@nofrixion/components'
+import r2wc from 'react-to-webcomponent'
+import React from 'react'
+
+const DashBoard = r2wc(AccountDashboard, React, ReactDOM, {
+  props: {
+    token: 'string',
+    apiUrl: 'string',
+    accountId: 'string',
+    merchantId: 'string',
+    onAllCurrentAccountsClick: 'function',
+  },
+})
+
+customElements.define('account-dashboard', DashBoard)
