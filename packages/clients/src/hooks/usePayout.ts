792c683af5934c76dce6b4f6dcc33b32b70117fd
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-09-06 13:59:08 +0100

    Added usePayout hook

--- packages/clients/src/hooks/usePayout.ts
+++ packages/clients/src/hooks/usePayout.ts
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { PayoutClient } from '../clients'
 import { ApiResponse, Payout, PayoutPageResponse } from '../types/ApiResponses'
-import { ApiProps, usePayoutsProps } from '../types/props'
+import { ApiProps, PayoutProps, usePayoutsProps } from '../types/props'
 
 const fetchPayout = async (
   apiUrl: string,
@@ -33,8 +33,11 @@ export const usePayout = (
     maxAmount,
     tags,
   }: usePayoutsProps,
+  { payoutId }: PayoutProps,
   { apiUrl, authToken }: ApiProps,
 ) => {
+  const queryClient = useQueryClient()
+
   const PAYOUTS_QUERY_KEY = [
     'Payouts',
     apiUrl,
@@ -55,29 +58,28 @@ export const usePayout = (
     tags,
   ]
 
-  return useQuery<ApiResponse<PayoutPageResponse>, Error>(
-    PAYOUTS_QUERY_KEY,
-    () =>
-      fetchPayout(
-        apiUrl,
-        statusSortDirection,
-        createdSortDirection,
-        amountSortDirection,
-        authToken,
-        merchantId,
-        pageNumber,
-        pageSize,
-        fromDateMS,
-        toDateMS,
-        status,
-        search,
-        currency,
-        minAmount,
-        maxAmount,
-        tags,
-      ),
-    {
-      enabled: !!merchantId,
+  const SINGLE_PAYOUT_QUERY_KEY = ['Payouts', payoutId, apiUrl, authToken]
+
+  return useQuery<ApiResponse<Payout>, Error>({
+    queryKey: SINGLE_PAYOUT_QUERY_KEY,
+    queryFn: () => fetchPayout(apiUrl, payoutId, authToken),
+    enabled: !!payoutId,
+    placeholderData: () => {
+      if (payoutId) {
+        const result: ApiResponse<PayoutPageResponse> | undefined =
+          queryClient.getQueryData<ApiResponse<PayoutPageResponse>>(PAYOUTS_QUERY_KEY)
+        if (result?.status === 'success') {
+          const payout: Payout | undefined = result.data.content.find((x) => x.id === payoutId)
+          if (payout) {
+            const apiresponse: ApiResponse<Payout> = {
+              data: payout,
+              status: 'success',
+              timestamp: new Date(),
+            }
+            return apiresponse
+          }
+        }
+      }
     },
-  )
+  })
 }
