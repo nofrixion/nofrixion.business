f7237a236f0df4bfb37fc51a967e21cfa77c3680
Axel Granillo <axel@nofrixion.com>
2023-09-27 14:37:03 -0600

    ✨ [MOOV-2084]: Restored dynamic horizontal navigation menu

---
+++ apps/business/src/components/Navbar/NavbarItem.tsx
@@ -0,0 +1,30 @@
+export interface NavItemProps {
+  leftIcon: any;
+  label: string;
+  href: string;
+  isActive?: boolean;
+}
+
+const NavItem: React.FC<NavItemProps> = ({
+    leftIcon,
+    label,
+    href,
+    isActive = false,
+  }) => {
+  return (
+    <a
+      href={href}
+      className="relative text-xs px-4 pt-2 pb-2 flex items-center 2xl:text-sm 2xl:px-6"
+    >
+      <div className="w-4 h-4">
+        <img src={leftIcon} alt={`${label} icon`} />
+      </div>
+
+      <span className="ml-3">{label}</span>
+
+      {isActive && <div className="h-1 bg-nav-accent w-full absolute left-0 bottom-0"></div>}
+    </a>
+  );
+};
+
+export default NavItem;
