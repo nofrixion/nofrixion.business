86cf3dcba86c7fcbe5051dfbbdb29c1a28e1ec08
Axel Granillo <axel@nofrixion.com>
2023-10-06 08:00:48 -0600

    ♻️ [MOOV-2174]: Added 'unknown' payment attempt payment status

--- packages/components/src/utils/parsers.ts
+++ packages/components/src/utils/parsers.ts
@@ -279,7 +279,7 @@ const remotePaymentRequestToLocalPaymentRequest = (
     if (remotePaymentAttempt.status === PaymentResult.FullyPaid) {
       return 'received'
     }
-    
+
     return 'unknown'
   }
 
