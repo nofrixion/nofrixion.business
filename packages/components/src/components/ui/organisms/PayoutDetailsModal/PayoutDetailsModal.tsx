44f40415b587328a577ebce4ed210f48da48ff2a
Axel Granillo <axel@nofrixion.com>
2023-10-09 08:50:28 -0600

    🐛 [MOOV-2343]: Fixed rough exit payout animation

--- packages/components/src/components/ui/organisms/PayoutDetailsModal/PayoutDetailsModal.tsx
+++ packages/components/src/components/ui/organisms/PayoutDetailsModal/PayoutDetailsModal.tsx
@@ -45,89 +45,91 @@ const PayoutDetailsModal = ({
         }}
         className="w-full lg:w-[37.5rem] outline-none"
       >
-        {payout && (
-          <div className="bg-white max-h-full h-full">
-            {payout && payout.status === PayoutStatus.PENDING_APPROVAL && (
-              <div className="flex bg-main-grey h-[72px] justify-end space-x-4 pr-8">
-                <div className="mt-4">
-                  <Button variant={'secondary'} size={'medium'} onClick={onDismiss}>
-                    Edit payout
-                  </Button>
+        <div className="bg-white max-h-full h-full">
+          {payout && (
+            <>
+              {payout && payout.status === PayoutStatus.PENDING_APPROVAL && (
+                <div className="flex bg-main-grey h-[72px] justify-end space-x-4 pr-8">
+                  <div className="mt-4">
+                    <Button variant={'secondary'} size={'medium'} onClick={onDismiss}>
+                      Edit payout
+                    </Button>
+                  </div>
+                  <div className="mt-4 mr-8">
+                    {payout && <PayoutAuthoriseForm id={payout?.id} size={'medium'} />}
+                  </div>
                 </div>
-                <div className="mt-4 mr-8">
-                  {payout && <PayoutAuthoriseForm id={payout?.id} size={'medium'} />}
-                </div>
-              </div>
-            )}
-            <div className="flex pt-10 mx-8 justify-between items-center">
-              <span className="text-[2rem] font-semibold leading-8 text-default-text tabular-nums pt-1">
-                {formatCurrency(payout?.currency ?? Currency.EUR)}
-                {amountValueWithCommas}
-                <sup className="ml-0.5 text-xl">.{amountDecimals}</sup>
-              </span>
+              )}
+              <div className="flex pt-10 mx-8 justify-between items-center">
+                <span className="text-[2rem] font-semibold leading-8 text-default-text tabular-nums pt-1">
+                  {formatCurrency(payout?.currency ?? Currency.EUR)}
+                  {amountValueWithCommas}
+                  <sup className="ml-0.5 text-xl">.{amountDecimals}</sup>
+                </span>
 
-              <Status size="large" variant={payoutStatusToStatus(payout.status)} />
-            </div>
-            <div className="mt-16 mx-8 w-11/12">
-              <div className="flex text-sm">
-                <div className="text-grey-text w-1/3">From account</div>
-                <div>
-                  <AccountDetails
-                    accountName={payout.sourceAccountName}
-                    accountNumber={
-                      payout.sourceAccountIban ??
-                      payout.sourceAccountNumber + ' ' + payout.sourceAccountSortCode
-                    }
-                  />
+                <Status size="large" variant={payoutStatusToStatus(payout.status)} />
+              </div>
+              <div className="mt-16 mx-8 w-11/12">
+                <div className="flex text-sm">
+                  <div className="text-grey-text w-1/3">From account</div>
+                  <div>
+                    <AccountDetails
+                      accountName={payout.sourceAccountName}
+                      accountNumber={
+                        payout.sourceAccountIban ??
+                        payout.sourceAccountNumber + ' ' + payout.sourceAccountSortCode
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="flex text-sm mt-8">
+                  <div className="text-grey-text w-1/3">To account</div>
+                  <div>
+                    <AccountDetails
+                      accountName={payout.destination?.name}
+                      accountNumber={
+                        payout.destination?.identifier?.iban ??
+                        payout.destination?.identifier?.accountNumber +
+                          ' ' +
+                          payout.destination?.identifier?.sortCode
+                      }
+                    />
+                  </div>
+                </div>
+                {payout.theirReference && (
+                  <div className="flex text-sm mt-8">
+                    <div className="text-grey-text w-1/3">Their reference</div>
+                    <div>{payout.theirReference}</div>
+                  </div>
+                )}
+                {payout.yourReference && (
+                  <div className="flex text-sm mt-8">
+                    <div className="text-grey-text w-1/3">Your reference</div>
+                    <div>{payout.yourReference}</div>
+                  </div>
+                )}
+                {payout.description && (
+                  <div className="flex text-sm mt-8">
+                    <div className="text-grey-text w-1/3">Description</div>
+                    <div className="w-2/3">{payout.description}</div>
+                  </div>
+                )}
+                <div className="flex text-sm mt-8">
+                  <div className="text-grey-text w-1/3">Tags</div>
+                  <div className="w-2/3">
+                    <TagManager
+                      availableTags={merchantTags}
+                      tags={payout.tags}
+                      onAdded={onTagAdded}
+                      onRemoved={onTagRemoved}
+                      onCreated={onTagCreated}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="flex text-sm mt-8">
-                <div className="text-grey-text w-1/3">To account</div>
-                <div>
-                  <AccountDetails
-                    accountName={payout.destination?.name}
-                    accountNumber={
-                      payout.destination?.identifier?.iban ??
-                      payout.destination?.identifier?.accountNumber +
-                        ' ' +
-                        payout.destination?.identifier?.sortCode
-                    }
-                  />
-                </div>
-              </div>
-              {payout.theirReference && (
-                <div className="flex text-sm mt-8">
-                  <div className="text-grey-text w-1/3">Their reference</div>
-                  <div>{payout.theirReference}</div>
-                </div>
-              )}
-              {payout.yourReference && (
-                <div className="flex text-sm mt-8">
-                  <div className="text-grey-text w-1/3">Your reference</div>
-                  <div>{payout.yourReference}</div>
-                </div>
-              )}
-              {payout.description && (
-                <div className="flex text-sm mt-8">
-                  <div className="text-grey-text w-1/3">Description</div>
-                  <div className="w-2/3">{payout.description}</div>
-                </div>
-              )}
-              <div className="flex text-sm mt-8">
-                <div className="text-grey-text w-1/3">Tags</div>
-                <div className="w-2/3">
-                  <TagManager
-                    availableTags={merchantTags}
-                    tags={payout.tags}
-                    onAdded={onTagAdded}
-                    onRemoved={onTagRemoved}
-                    onCreated={onTagCreated}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+            </>
+          )}
+        </div>
       </SheetContent>
     </Sheet>
   )
