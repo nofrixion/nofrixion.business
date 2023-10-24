0cae60fd6cea28ceaeee717a92fbdfd185fcd978
Pablo Maldonado <pablomartinmaldonado1@gmail.com>
2023-10-24 12:55:14 -0300

    💄 fix: refactor Latest Transaction Row loading

--- packages/components/src/components/ui/molecules/LatestTransactionsCard/LatestTransactionsCard.tsx
+++ packages/components/src/components/ui/molecules/LatestTransactionsCard/LatestTransactionsCard.tsx
@@ -11,22 +11,29 @@ export interface LatestTransactionsCardProps {
   className?: string
 }
 
-interface TransactionRowWithTransaction {
-  transaction: LocalTransaction
-  isLoading?: never
+type TransactionRowProps = { transaction: LocalTransaction }
+
+const LoadingTransactionRow: React.FC = () => {
+  const loadingClass = 'bg-[#EFF4F5] animate-pulse rounded-lg h-2'
+  return (
+    <div
+      className={cn(
+        'flex flex-row md:items-center py-4 border-border-grey space-x-6 xl:space-x-9 w-full h-6 flex-1',
+      )}
+    >
+      <div className="flex flex-col flex-1 md:flex-none md:flex-row-reverse">
+        <span className={cn('w-40', loadingClass)} />
+        <span className={cn('md:mr-6 md:w-24 xl:mr-9', loadingClass)} />
+      </div>
+
+      <span className={cn('hidden md:block w-28', loadingClass)} />
+
+      <span className={cn('!ml-auto w-20', loadingClass)} />
+    </div>
+  )
 }
-interface TransactionRowWithLoading {
-  transaction?: never
-  isLoading: true
-}
 
-type TransactionRowProps = TransactionRowWithTransaction | TransactionRowWithLoading
-
-const TransactionRow: React.FC<TransactionRowProps> = ({ transaction: tx, isLoading }) => {
-  const loadingClasses = 'bg-[#E0E9EB] animate-pulse rounded-md h-3'
-
-  const mergedClasses = (classes: string) => cn(classes, isLoading && loadingClasses)
-
+const TransactionRow: React.FC<TransactionRowProps> = ({ transaction: tx }) => {
   return (
     <div
       className={cn(
@@ -34,34 +41,23 @@ const TransactionRow: React.FC<TransactionRowProps> = ({ transaction: tx, isLoad
       )}
     >
       <div className="flex flex-col flex-1 md:flex-none md:flex-row-reverse">
-        <span className={mergedClasses('text-13px/4 w-40 md:leading-6')}>
-          {!isLoading && tx.counterParty.name}
-        </span>
-        <span
-          className={mergedClasses(
-            'text-13px/4 text-grey-text md:leading-6 md:mr-6 md:w-24 xl:mr-9',
-          )}
-        >
-          {!isLoading && formatDate(tx.date)}
+        <span className="text-13px/4 w-40 md:leading-6">{tx.counterParty.name}</span>
+        <span className={'tet-13px/4 text-grey-text md:leading-6 md:mr-6 md:w-24 xl:mr-9'}>
+          {formatDate(tx.date)}
         </span>
       </div>
 
-      <span className={mergedClasses('hidden md:block flex-1')}>
-        {!isLoading && (tx.accountName ?? '-')}
-      </span>
+      <span className="hidden md:block flex-1">{tx.accountName ?? '-'}</span>
 
       <span
-        className={mergedClasses(
-          cn(
-            'text-sm/4 md:text-sm/6 font-medium tabular-nums text-right ml-auto',
-            !isLoading && tx.amount >= 0 ? 'text-positive-green' : 'text-negative-red',
-          ),
-        )}
+        className={cn('text-sm/4 md:text-sm/6 font-medium tabular-nums text-right ml-auto', {
+          'text-positive-green': tx.amount >= 0,
+          'text-negative-red': tx.amount < 0,
+        })}
       >
-        {!isLoading &&
-          `${tx.amount < 0 ? '-' : ''} ${formatCurrency(tx.currency)}${formatAmount(
-            Math.abs(tx.amount),
-          )}`}
+        {`${tx.amount < 0 ? '-' : ''} ${formatCurrency(tx.currency)}${formatAmount(
+          Math.abs(tx.amount),
+        )}`}
       </span>
     </div>
   )
@@ -73,15 +69,9 @@ const LatestTransactionsCard: React.FC<LatestTransactionsCardProps> = ({
   className,
 }) => (
   <Card title="Latest transactions" className={className}>
-    <div
-      className={cn('mt-8 md:mt-10 w-full', {
-        'divide-y': !isLoading,
-      })}
-    >
+    <div className="mt-8 md:mt-10 w-full divide-y">
       {isLoading &&
-        Array.from({ length: 9 }).map((_, i) => (
-          <TransactionRow key={`loading-tx-${i}`} isLoading />
-        ))}
+        Array.from({ length: 9 }).map((_, i) => <LoadingTransactionRow key={`loading-tx-${i}`} />)}
 
       {/* Emtpy state - if no transactions have happened yet */}
       {!isLoading && (!transactions || transactions.length == 0) && (
