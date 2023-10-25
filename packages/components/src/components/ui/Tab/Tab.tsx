f01f0141be0d5c414a92525f7bcd2ed0204776fb
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-10-25 10:49:59 +0100

    Adjusted padding on tabs

--- packages/components/src/components/ui/Tab/Tab.tsx
+++ packages/components/src/components/ui/Tab/Tab.tsx
@@ -105,7 +105,7 @@ const Tab = ({
     <Tabs.Trigger
       value={status}
       className={classNames(
-        'flex flex-col items-center xl:items-start w-36 h-20 px-2 pt-2 pb-4 rounded-lg lg:w-full lg:h-28 xl:px-8 lg:pt-6 lg:pb-8 bg-white border-2 border-transparent transition hover:border-border-grey',
+        'flex flex-col items-center xl:items-start w-36 h-20 px-2 pt-2 pb-4 rounded-lg lg:w-full lg:h-28 xl:pl-6 xl:pr-4 lg:pt-6 lg:pb-8 bg-white border-2 border-transparent transition hover:border-border-grey',
         getSpecificStatusClasses(status),
       )}
     >
