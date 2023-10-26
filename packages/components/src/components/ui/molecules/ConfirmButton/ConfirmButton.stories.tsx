15042a8a757fcc09850f785ab93a58a49467d2a8
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-10-26 09:33:19 +0100

    Added confirm button component

--- packages/components/src/components/ui/molecules/ConfirmButton/ConfirmButton.stories.tsx
+++ packages/components/src/components/ui/molecules/ConfirmButton/ConfirmButton.stories.tsx
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions'
 import { Meta, StoryFn } from '@storybook/react'
 
 import ConfirmButton from './ConfirmButton'
@@ -5,6 +6,11 @@ import ConfirmButton from './ConfirmButton'
 export default {
   title: 'Molecules/Confirm Button',
   component: ConfirmButton,
+  argTypes: {
+    primaryText: { control: 'text' },
+    confirmText: { control: 'text' },
+    onConfirm: { action: 'onConfirm' },
+  },
 } as Meta
 
 const Template: StoryFn<typeof ConfirmButton> = (args) => <ConfirmButton {...args} />
@@ -13,4 +19,5 @@ export const Default = Template.bind({})
 Default.args = {
   primaryText: 'Cancel authorisation',
   confirmText: 'Click again to confirm',
+  onConfirm: action('onConfirm'),
 }
--- packages/components/src/components/ui/molecules/ConfirmButton/ConfirmButton.tsx
+++ packages/components/src/components/ui/molecules/ConfirmButton/ConfirmButton.tsx
@@ -1,3 +1,6 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useState } from 'react'
+
 import { Button } from '../../atoms'
 
 export interface ConfrimButtonProps {
@@ -7,17 +10,49 @@ export interface ConfrimButtonProps {
 }
 
 const ConfrimButton = ({ primaryText, confirmText, onConfirm }: ConfrimButtonProps) => {
+  const [isConfirming, setIsConfirming] = useState<boolean>(false)
+
+  const handleKeyDown = (evt: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault()
+      evt.currentTarget.blur()
+      setIsConfirming(false)
+    }
+  }
+
   return (
-    <>
-      <div className="flex-row">
-        <Button variant={'secondary'} onClick={onConfirm} className="w-[189px]">
-          {primaryText}
-        </Button>
-      </div>
-      <Button variant={'confirm_negative'} onClick={onConfirm} className="w-[189px]">
-        {confirmText}
-      </Button>
-    </>
+    <AnimatePresence>
+      {!isConfirming && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <Button variant={'secondary'} onClick={() => setIsConfirming(true)} className="w-[189px]">
+            {primaryText}
+          </Button>
+        </motion.div>
+      )}
+      {isConfirming && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <Button
+            variant={'confirm_negative'}
+            onClick={onConfirm}
+            onKeyDown={(event) => handleKeyDown(event)}
+            onBlur={() => setIsConfirming(false)}
+            className="w-[189px]"
+          >
+            {confirmText}
+          </Button>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
