dfda486cc47338ffcc86cae6eed10de06b03c42c
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-11-10 17:23:10 +0000

    Added NotifyModal component
    Updated AppLogout to use NotifyModal

--- apps/business/src/lib/auth/AppLogout.tsx
+++ apps/business/src/lib/auth/AppLogout.tsx
@@ -1,83 +1,118 @@
+import NotifyModal from '@nofrixion/components/src/components/ui/molecules/NotifyModal/NotifyModal'
 import { LocalUserRoles } from '@nofrixion/components/src/types/LocalTypes'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useIdleTimer } from 'react-idle-timer'
 
 import useUserStore from '../stores/useUserStore'
 import { AuthContextType } from './AuthProvider'
 import { useAuth } from './useAuth'
-
-// const events = ['load', 'mousemove', 'mousedown', 'click', 'scroll', 'keypress']
-
 interface AppLogoutProps {
   children: React.ReactNode
 }
 
-const timeout = 10_000
-const promptBeforeIdle = 4_000
-
-// TODO: Review this. We need to get user role from the API
-// and possibly not enable the timer if user is a PaymentRequester?
 const AppLogout = ({ children }: AppLogoutProps) => {
-  // const timer = useRef<NodeJS.Timeout | undefined>(undefined)
   const { authState } = useAuth() as AuthContextType
-  let expiryTimeMs = 1000 * 60 * 5 // 5 minutes
+  const { user } = useUserStore()
 
-  // const [state, setState] = useState<string>('Active')
-  // const [remaining, setRemaining] = useState<number>(timeout)
-  // const [open, setOpen] = useState<boolean>(false)
+  const [state, setState] = useState<'Active' | 'Prompted' | 'Idle'>('Active')
+  const [remainingSeconds, setRemainingSeconds] = useState<number>(0)
+  const [open, setOpen] = useState<boolean>(false)
 
+  const timeout = user?.role === LocalUserRoles.PaymentRequestor ? 1000 * 60 * 30 : 1000 * 60 * 5
+  const promptBeforeIdle = 1000 * 60
+
+  useEffect(() => {
+    if (state === 'Prompted') {
+      document.title = `${remainingSeconds}s to end session`
+    }
+  }, [remainingSeconds])
+
+  let interval: NodeJS.Timer
+
+  useEffect(() => {
+    if (state === 'Prompted') {
+      interval = setInterval(() => {
+        setRemainingSeconds(Math.ceil(getRemainingTime() / 1000))
+      }, 500)
+    } else {
+      clearInterval(interval)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [state])
+
+  // Logout the user
   const onIdle = () => {
-    // setState('Idle')
-    // setOpen(false)
-    console.log('user is idle')
-    console.log('last active', getRemainingTime())
+    setState('Idle')
+    setOpen(false)
+    document.title = 'NoFrixion'
     logout()
   }
 
-  const onActive = () => {
-    // setState('Active')
-    // setOpen(false)
-    console.log('user is active')
-  }
-
+  // Prompt the user before 1 min before the session ends
   const onPrompt = () => {
-    // setState('Prompted')
-    // setOpen(true)
-    console.log('user is being prompted')
+    setState('Prompted')
+    setOpen(true)
   }
 
-  const { getRemainingTime, activate } = useIdleTimer({
+  const onActive = () => {
+    setState('Active')
+    setOpen(false)
+  }
+
+  // Keep the user logged in by resetting the timer
+  const stayLoggedIn = () => {
+    document.title = 'NoFrixion'
+    setState('Active')
+    setOpen(false)
+    start()
+  }
+
+  // Create the idle timer
+  const { getRemainingTime, start } = useIdleTimer({
     onIdle,
-    onActive,
     onPrompt,
+    onActive,
     timeout,
     promptBeforeIdle,
     throttle: 500,
     startManually: true,
+    crossTab: true,
+    syncTimers: 200,
   })
 
-  console.log('Remaining time', getRemainingTime())
-  const { user } = useUserStore()
-
-  if (user?.role === LocalUserRoles.PaymentRequestor) {
-    expiryTimeMs = 1000 * 60 * 0.5
-  }
-
+  // Logout
   const logout = useCallback(
     (callbackUrl?: string) => {
-      // resetTimer()
-      authState?.logOut && authState.logOut(callbackUrl)
+      if (authState?.isLoggedIn) {
+        authState?.logOut && authState.logOut(callbackUrl)
+      }
     },
     [authState],
   )
 
+  // Start the idle timer when the user is logged in
   useEffect(() => {
     if (authState?.isLoggedIn) {
-      activate()
+      start()
     }
-  }, [expiryTimeMs, authState?.isLoggedIn])
+  }, [authState?.isLoggedIn])
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      {remainingSeconds > 0 && (
+        <NotifyModal
+          open={open}
+          onApply={stayLoggedIn}
+          notifyText={`Your session is ending in ${remainingSeconds} seconds`}
+          buttonText="Keep me logged in"
+        />
+      )}
+    </>
+  )
 }
 
 export default AppLogout
---
+++ packages/components/src/components/ui/molecules/NotifyModal/NotifyModal.tsx
@@ -0,0 +1,51 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { AnimatePresence, motion } from 'framer-motion'
+
+import { Button } from '../../atoms'
+
+export interface NotifyModalProps {
+  open: boolean
+  notifyText: string
+  buttonText: string
+  onApply: () => void
+}
+
+const NotifyModal = ({ open, notifyText, buttonText, onApply }: NotifyModalProps) => {
+  return (
+    <Dialog.Root open={open}>
+      <AnimatePresence>
+        {open && (
+          <Dialog.Portal forceMount>
+            <Dialog.Overlay forceMount>
+              <motion.div
+                className="fixed inset-0 bg-black bg-opacity-25 z-50"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              />
+            </Dialog.Overlay>
+            <Dialog.Content
+              forceMount
+              className="fixed top-[50%] left-[50%] w-[528px] translate-x-[-50%] translate-y-[-50%] z-50"
+            >
+              <div className="flex flex-col min-h-full justify-center items-center overflow-hidden rounded-lg bg-white align-middle shadow-xl transition-all text-default-text text-2xl font-semibold">
+                <span className="md:px-12 mt-12 text-center max-w-[390px]">{notifyText}</span>
+                <Button
+                  variant="primaryDark"
+                  size="large"
+                  onClick={onApply}
+                  className="mx-auto mt-8 mb-12 w-fit"
+                >
+                  {buttonText}
+                </Button>
+              </div>
+            </Dialog.Content>
+          </Dialog.Portal>
+        )}
+      </AnimatePresence>
+    </Dialog.Root>
+  )
+}
+
+export default NotifyModal
