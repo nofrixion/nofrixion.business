dce2b6503ab2a02139e86305d03d8f9c37893ad8
donalnofrixion <89344328+donalnofrixion@users.noreply.github.com>
2023-11-10 09:19:13 +0000

    Adjusted padding

--- apps/business/src/pages/NewlyRegistered.tsx
+++ apps/business/src/pages/NewlyRegistered.tsx
@@ -14,14 +14,16 @@ const NewlyRegistered = () => {
     <div className="flex flex-col">
       <div className="pl-10 text-[32px] font-semibold">{`${user?.firstName}, welcome to MoneyMoov for Business`}</div>
       <div className="flex rounded-lg mt-16 px-10 py-8 bg-white w-full">
-        <Icon name="info/16" className="stroke:control-grey-hover mt-1" />
+        <div className=" mt-1 w-4 h-4">
+          <Icon name="info/16" className="stroke:control-grey-hover" />
+        </div>
 
         <div className="flex flex-col pl-4">
           <div className="text-default-text text-base font-normal">
             Your system administrator has been notified of your login and will activate your account
             soon.
           </div>
-          <div className="text-sm font-normal pt-4 text-grey-text">
+          <div className="text-sm font-normal pt-2 text-grey-text">
             You will receive an email notification when your access is ready.
           </div>
         </div>
